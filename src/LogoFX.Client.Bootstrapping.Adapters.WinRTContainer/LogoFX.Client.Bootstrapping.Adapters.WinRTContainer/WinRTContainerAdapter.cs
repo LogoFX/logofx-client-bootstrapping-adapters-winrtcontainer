8252d21d77855297c3d1f1906b025c41813bd6ee
godrose <godrose@gmail.com>
2016-03-09 16:10:55 +0200

    Added initial solution.

---
+++ src/LogoFX.Client.Bootstrapping.Adapters.WinRTContainer/LogoFX.Client.Bootstrapping.Adapters.WinRTContainer/WinRTContainerAdapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using LogoFX.Client.Bootstrapping.Adapters.Contracts;
+
+namespace LogoFX.Client.Bootstrapping.Adapters.WinRTContainer
+{
+    public class WinRTContainerAdapter : IBootstrapperAdapter
+    {
+        private readonly Caliburn.Micro.WinRTContainer _container;
+
+        public WinRTContainerAdapter()
+            :this(new Caliburn.Micro.WinRTContainer())
+        {
+            
+        }
+
+        public WinRTContainerAdapter(Caliburn.Micro.WinRTContainer container)
+        {
+            _container = container;
+        }
+
+        public object GetInstance(Type serviceType, string key)
+        {
+            return _container.GetInstance(serviceType, key);
+        }
+
+        public IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return _container.GetAllInstances(serviceType);
+        }
+
+        public void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
