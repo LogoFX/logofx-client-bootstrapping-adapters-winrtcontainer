20a1e687c91c54b559a121095360986a9ee8fe13
godrose <godrose@gmail.com>
2016-04-11 09:48:22 +0300

    Added win8.1 and windows phone (winrt-based) 8.1 platforms support.

---
+++ src/LogoFX.Client.Bootstrapping.Adapters.WinRTContainer/src/INavigationAdapter.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace LogoFX.Client.Bootstrapping.Adapters.WinRTContainer
+{
+    /// <summary>
+    /// Represents an adapter to Navigation using Caliburn.Micro facilities.
+    /// </summary>
+    public interface INavigationAdapter
+    {
+        /// <summary>
+        /// Sets the specified frame as the root frame of the navigation.
+        /// </summary>
+        /// <param name="frame">The root frame</param>
+        void RegisterNavigationService(Frame frame);
+    }
+}
